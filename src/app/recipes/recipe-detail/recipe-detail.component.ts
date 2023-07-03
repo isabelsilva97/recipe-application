63078bb60e7d2368255032fdbd4a27e23ef00930
Ana Isabel Silva <106960839+isabelsilva97@users.noreply.github.com>
2023-07-03 17:52:28 +0100

    Components added to app-component.ts

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-recipes-detail',
+  selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css']
 })
